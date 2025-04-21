d2055cb01285b991f0e25a81d4c370bed77c245c
Ahkar Min Myat <opstan98@gmail.com>
2025-04-21 17:21:52 +0700

    pk batch load

---
+++ LoadPK.sql
@@ -0,0 +1,23 @@
+USE [CMS_KBANK]
+GO
+
+DECLARE @SQL VARCHAR(MAX) = '';
+
+-- Loop through all user tables
+SELECT @SQL = @SQL + 
+'ALTER TABLE ' + QUOTENAME(TABLE_NAME) + 
+' ADD CONSTRAINT PK_' + TABLE_NAME + 
+' PRIMARY KEY (' + QUOTENAME(COLUMN_NAME) + ');' + CHAR(10)
+FROM (
+    SELECT 
+        TABLE_NAME, 
+        COLUMN_NAME, 
+        ROW_NUMBER() OVER (PARTITION BY TABLE_NAME ORDER BY ORDINAL_POSITION) AS RowNum
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'dbo' -- Adjust schema if needed
+) AS FirstColumns
+WHERE RowNum = 1; -- Select only the first column
+
+-- Print or execute
+PRINT @SQL
+-- EXEC sp_executesql @SQL -- Uncomment to execute
