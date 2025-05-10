1a0348a35ecbe8a31d3aee62174a89537c5ffafb
Ahkar Min Myat <opstan98@gmail.com>
2025-05-10 19:59:22 +0700

    delete old backup 7 days #v1.1

---
+++ Backup/RemoveOldBackup7Day.sql
@@ -0,0 +1,23 @@
+-- delete old backup
+USE [master]
+GO
+
+-- Step 1: Enable xp_cmdshell
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'xp_cmdshell', 1;
+RECONFIGURE;
+
+-- Step 2: Call the PowerShell script
+DECLARE @psCommand NVARCHAR(4000);
+
+-- Change this path to your PowerShell script location
+SET @psCommand = 'powershell.exe -ExecutionPolicy Bypass -File "E:\T-SQL\RemoveOldBackup7Day.ps1"';
+
+EXEC xp_cmdshell @psCommand;
+
+-- Step 3: Disable xp_cmdshell again
+EXEC sp_configure 'xp_cmdshell', 0;
+RECONFIGURE;
+EXEC sp_configure 'show advanced options', 0;
+RECONFIGURE;
